261065e254e45485e163d547b51233afc85e6e1f
GeorgesSemedoCabral <109231432+GeorgesSemedoCabral@users.noreply.github.com>
2022-09-23 23:18:42 +0200

    included into code_modelisation

--- api/split_test_df.py
+++
@@ -1,6 +0,0 @@
-import joblib
-import pandas as pd
-
-for i, chunk in enumerate(pd.read_csv("preprocessed_test_df.csv",
-                          chunksize=10000)):
-    joblib.dump(chunk, "data/split_csv_pandas/chunk{}.sav".format(i))
